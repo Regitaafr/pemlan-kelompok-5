844542a4c102092e02b57753b3c353459036dcaa
Regitaafr <regitaafrr@gmail.com>
2022-10-03 21:40:35 +0700

    first commit

---
+++ Program Pemlan.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+main ()
+{
+	int bil;
+
+	printf("\n-----------------------\n");
+  	printf("\n 1.Materi Kelas VII\n");
+ 	printf("\n 2.Materi Kelas VIII\n");
+  	printf("\n 3.Materi Kelas IX\n");
+  	printf("\n-----------------------\n");
+  
+  	printf("Masukan pilihan anda: ");
+  	scanf("%d" , &bil);
+  	
+  	if (bil == 1) {
+  		printf("\n-----------------------\n");
+  		printf("Materi Kelas VII");
+  		printf("\n-----------------------\n");
+  		printf("1. Bilangan Bulat dan Pecahan\n");
+  		printf("2. Himpunan\n");
+  		printf("3. Persamaan dan Pertidaksamaan Linear satu Variabel\n");
+  		printf("4. Perbandingan\n");
+  		printf("5. Aritmatika Sosial\n");
+  		printf("6. Segiempat\n");
+  		printf("7. Segitiga\n");
+  		printf("8. Penyajian Data\n");
+  		printf("Materi yang dipilih: ");
+  		scanf("%d" , &bil);
+  		
+  	}
+  	
+}
